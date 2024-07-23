617ec8bf08fbc664c2ff8ba049c7b080cc206232
alsahas <168373030+alsahas@users.noreply.github.com>
2024-07-23 21:41:30 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+alert("okk");
+var nodemailer = require('nodemailer');
+alert("okk");
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'youremail@gmail.com',
+    pass: 'yourpassword'
+  }
+});
+
+var mailOptions = {
+  from: 'youremail@gmail.com',
+  to: 'myfriend@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  text: 'That was easy!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
