05a1c8027ab93adb5f35c059ecd9e0f3b46ed2a1
alsahas <168373030+alsahas@users.noreply.github.com>
2024-08-04 22:41:30 +0300

    Delete java.js

--- java.js
+++
@@ -1,6 +0,0 @@
-function SendMail()
-{
-    var body = document.getElementById("message").value;
-    var SubjectLine = document.getElementById("subject").value;
-    window.location.href = "mailto:strategyquant1@gmail.com?subject="+SubjectLine+"&body="+body;
-}
